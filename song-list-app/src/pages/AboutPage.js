df8dfc8a417e058b3d285a7366f0f115fa8a6020
Shalom Habtamu <shalomhabtamum@gmail.com>
2024-07-02 16:44:04 +0300

    Edited Navigation

--- song-list-app/src/pages/AboutPage.js
+++ song-list-app/src/pages/AboutPage.js
@@ -4,13 +4,16 @@ import aboutImage from '../assets/about-image.jpg';
 
 const AboutPage = () => {
   return (
-    <div className="about-page">
+    <div className="about-page" id="about">
       <div className="left">
         <h1>About Us</h1>
         <p>
           Welcome to our music world! AlphaMusic is passionate about bringing you the best music
           experience. Our mission is to connect people through the love of music. Explore
           our collection and join us on this musical journey.
+          Discover the rhythm and harmony at AlphaMusic! We strive to provide an unparalleled musical 
+          experience. Our goal is to unite music enthusiasts worldwide. Dive into our vast collection and 
+          embark on a musical adventure with us.
         </p>
       </div>
       <div className="right">
--- song-list-app/src/pages/HomePage.js
+++ song-list-app/src/pages/HomePage.js
@@ -1,20 +1,19 @@
 import React from 'react';
 import './HomePage.css';
 
-
 const HomePage = () => (
-  <div className="home-page">
+  <div className="home-page" id="home">
     <div className="nav-bar">
       <div>
         <a href="#home">HOME</a>
-        <a href="#listen">ABOUT</a>
+        <a href="#about">ABOUT</a>
       </div>
       <div>
         <span>ALPHA MUSIC</span>
       </div>
       <div>
-        <a href="#bookings">SONGS</a>
-        <a href="#tour">CONTACT</a>
+        <a href="#songs">SONGS</a>
+        <a href="#contact">CONTACT</a>
       </div>
     </div>
     <div className="content">
