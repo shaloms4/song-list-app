1f9014862349d3c935bb98965596e41727675149
Shalom Habtamu <shalomhabtamum@gmail.com>
2024-07-02 16:54:13 +0300

    Edited Color

--- song-list-app/src/pages/HomePage.js
+++ song-list-app/src/pages/HomePage.js
@@ -1,26 +1,45 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './HomePage.css';
 
-const HomePage = () => (
-  <div className="home-page" id="home">
-    <div className="nav-bar">
-      <div>
-        <a href="#home">HOME</a>
-        <a href="#about">ABOUT</a>
+const HomePage = () => {
+  useEffect(() => {
+    const handleScroll = () => {
+      const navBar = document.querySelector('.nav-bar');
+      if (window.scrollY > 50) {
+        navBar.classList.add('scrolled');
+      } else {
+        navBar.classList.remove('scrolled');
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  return (
+    <div className="home-page" id="home">
+      <div className="nav-bar">
+        <div>
+          <a href="#home">HOME</a>
+          <a href="#about">ABOUT</a>
+        </div>
+        <div>
+          <span>ALPHA MUSIC</span>
+        </div>
+        <div>
+          <a href="#songs">SONGS</a>
+          <a href="#contact">CONTACT</a>
+        </div>
       </div>
-      <div>
-        <span>ALPHA MUSIC</span>
-      </div>
-      <div>
-        <a href="#songs">SONGS</a>
-        <a href="#contact">CONTACT</a>
+      <div className="content">
+        <h1 className="main-heading">NEW ALBUM OUT</h1>
+        <button className="button">LISTEN NOW</button>
       </div>
     </div>
-    <div className="content">
-      <h1 className="main-heading">NEW ALBUM OUT</h1>
-      <button className="button">LISTEN NOW</button>
-    </div>
-  </div>
-);
+  );
+};
 
 export default HomePage;
