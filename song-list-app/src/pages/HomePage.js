46b75c6259084a52834cd4d206a3cb16accda996
Shalom Habtamu <shalomhabtamum@gmail.com>
2024-07-03 17:18:12 +0300

    Added Hamburger Menu

--- song-list-app/src/pages/HomePage.js
+++ song-list-app/src/pages/HomePage.js
@@ -1,7 +1,9 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import './HomePage.css';
 
 const HomePage = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   useEffect(() => {
     const handleScroll = () => {
       const navBar = document.querySelector('.nav-bar');
@@ -21,18 +23,29 @@ const HomePage = () => {
 
   return (
     <div className="home-page" id="home">
-      <div className="nav-bar">
-        <div>
+      <div className={`nav-bar ${isMenuOpen ? 'menu-open' : ''}`}>
+        <div className="nav-left">
           <a href="#home">HOME</a>
           <a href="#about">ABOUT</a>
         </div>
-        <div>
+        <div className="nav-center">
           <span>ALPHA MUSIC</span>
         </div>
-        <div>
+        <div className="nav-right">
           <a href="#songs">SONGS</a>
           <a href="#contact">CONTACT</a>
         </div>
+        <button className="hamburger-menu" onClick={() => setIsMenuOpen(!isMenuOpen)}>
+          <span className="bar"></span>
+          <span className="bar"></span>
+          <span className="bar"></span>
+        </button>
+      </div>
+      <div className={`menu ${isMenuOpen ? 'open' : ''}`}>
+        <a href="#home" onClick={() => setIsMenuOpen(false)}>HOME</a>
+        <a href="#about" onClick={() => setIsMenuOpen(false)}>ABOUT</a>
+        <a href="#songs" onClick={() => setIsMenuOpen(false)}>SONGS</a>
+        <a href="#contact" onClick={() => setIsMenuOpen(false)}>CONTACT</a>
       </div>
       <div className="content">
         <h1 className="main-heading">NEW ALBUM OUT</h1>
